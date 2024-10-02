f1ffd4ac6ea623c8af92cc25de7c58b3f95ef82b
Shamonti <shamonti.haque98@gmail.com>
2024-10-02 17:37:45 +0600

    Added comments api

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
+# from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, AddCommentView, ListCommentView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
+# router.register(r'comments', CommentViewSet, basename = 'comment')
 
 urlpatterns = [
   # path("posts", PostListView.as_view(), name='post-list'),
   path('', include(router.urls)),
+  path('posts/<int:post_id>/comments/', ListCommentView.as_view(), name='list-comment'),
+  path('comments/add', AddCommentView.as_view(), name='add-comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 # from rest_framework.views import APIView
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 # from rest_framework.response import Response
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
 # class PostListView(APIView):
 #   def get(self, request):
@@ -16,3 +16,18 @@ from .serializers import PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
+
+# class CommentViewSet(viewsets.ModelViewSet):
+#   queryset = Comment.objects.all()
+#   serializer_class = CommentSerializer
+
+class AddCommentView(generics.CreateAPIView):
+  queryset = Comment.objects.all()
+  serializer_class = CommentSerializer
+
+class ListCommentView(generics.ListAPIView):
+  serializer_class = CommentSerializer
+
+  def get_queryset(self):
+      post_id = self.kwargs['post_id']  # Extract the post ID from the URL
+      return Comment.objects.filter(post__id=post_id)
