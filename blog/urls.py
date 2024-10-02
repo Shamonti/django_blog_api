b843e987ed809e619d759c27db303e7000758bb8
Shamonti <shamonti.haque98@gmail.com>
2024-10-02 16:02:45 +0600

    Build CRUD Operations for Blog Posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import PostListView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
 
 urlpatterns = [
-  path("posts", PostListView.as_view(), name='post-list'),
+  # path("posts", PostListView.as_view(), name='post-list'),
+  path('', include(router.urls)),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.views import APIView
-from rest_framework.response import Response
+# from rest_framework.views import APIView
+from rest_framework import viewsets
+# from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
 
-class PostListView(APIView):
-  def get(self, request):
-    posts = Post.objects.all()
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+# class PostListView(APIView):
+#   def get(self, request):
+#     posts = Post.objects.all()
+#     serializer = PostSerializer(posts, many=True)
+#     return Response(serializer.data)
+
+class PostViewSet(viewsets.ModelViewSet):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
